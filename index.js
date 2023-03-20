fdb3d6c54f7a7910460ea06f0c634a187392adac
marcengarcia <marcengarcia@gmail.com>
2023-03-20 20:52:22 -0300

    take 2

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
+const express = require ('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const path = require('path')
+
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname,'public')))
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/',function(err,res) {
+    res.render('home', {})
+})
 
 
-app.listen(PORT, () => console.log("server listening on port: "+ PORT))
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(PORT, () => console.log('Server listening on port: ' + PORT ))
